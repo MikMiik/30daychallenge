b023faf775d3b9e73357d7f80e33a64e842b743f
Pham Van Minh <minh0936532430@gmail.com>
2025-03-15 12:24:46 +0700

    Done but just click to render

--- WeatherApp/weather.js
+++ WeatherApp/weather.js
@@ -1,5 +1,9 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+const container = $('.container');
+const centre = $('.centre');
+const centreDetails = $('.centre .weather-details');
+const footer = $('.footer');
 const searchInput = $('.search-input');
 const searchBtn = $('.search-box button');
 const temperature = $('.temperature');
@@ -12,20 +16,30 @@ async function fetchData(city) {
     try {
         const responseCoordinates = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}}&limit=1&appid=979322e8a48b71c2eb169a5af81da036`);
         const coordinatesData = await responseCoordinates.json();
+        if (coordinatesData.length == 0) return weatherData = '';
         const latitude = coordinatesData[0].lat;
         const longtitude = coordinatesData[0].lon;
         const responseWeatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=979322e8a48b71c2eb169a5af81da036`);
         const weatherData = await responseWeatherData.json();
         return weatherData;
     } catch (error) {
-        console.error('Lỗi', error)
+        console.log('Lỗi')
     }
 }
 
 searchBtn.addEventListener('click', () => {
     const city = searchInput.value.trim();
+    if (city == '') return;
     fetchData(city).then((weatherData) =>  {
         console.log(weatherData)
+        if (!weatherData) {
+            weatherImg.src = 'notfound.png';
+            container.style.height = '400px';
+            centre.classList.add('fade-in');
+            footer.classList.remove('fade-in');
+            centreDetails.innerHTML = '<p>Oops! Invalid location :/</p>'
+            return;
+        }
         switch ( weatherData.weather[0].main) {
             case 'Clouds':
                 weatherImg.src = 'cloud.png';
@@ -56,11 +70,15 @@ searchBtn.addEventListener('click', () => {
 
             default: weatherImg.src = '';
         }
+        container.style.height = '540px';
+        centre.classList.add('fade-in');
+        footer.classList.add('fade-in');
 
-        temperature.innerText = `${parseInt(weatherData.main.temp) - 273}°C`;
-        describe.innerText = weatherData.weather[0].main;
+        centreDetails.innerHTML = 
+        `<p class="temperature">${parseInt(weatherData.main.temp) - 273}°C</p>
+        <p class="describe">${weatherData.weather[0].main}</p>
+        `
         humidityData.innerText = `${weatherData.main.humidity}%`;
-        windData.innerText = `${weatherData.wind.speed}Km/h`
-
+        windData.innerText = `${weatherData.wind.speed}Km/h`;
     });
 })
